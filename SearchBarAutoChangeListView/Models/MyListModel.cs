a8877e2797677d9f2a5572be6d205fdca1f702e9
vlmta <vlmtam@outlook.com>
2020-08-30 22:29:58 +0700

    Add project files.

---
+++ SearchBarAutoChangeListView/Models/MyListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchBarAutoChangeListView.Models
+{
+    public class MyListModel
+    {
+        public string MyName { get; set; }
+        public string MyDetail { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ SearchBarAutoChangeListView/Views/MyListPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using SearchBarAutoChangeListView.ViewModel;
+using Xamarin.Forms;
+
+namespace MySearchList.Views
+{
+
+    public partial class MyListPage : ContentPage
+    {
+
+        public MyListPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MyListViewModel();
+
+        }
+
+        void Handle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var _container = BindingContext as MyListViewModel;
+            EmployeeListView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                EmployeeListView.ItemsSource = _container.MyListCollector;
+            else
+                EmployeeListView.ItemsSource = _container.MyListCollector.Where(i => i.MyName.Contains(e.NewTextValue));
+
+            EmployeeListView.EndRefresh();
+        }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var _container = BindingContext as MyListViewModel;
+            //do work over here
+            DisplayAlert("Sucess", "You have Subscribed", "OK", "Cancel");
+        }
+    }
+}
